0ad1a5c0641f729d786bd4f1ef07256f71a194fc
AndresFTR <afelipe0113@gmail.com>
2019-07-03 08:53:11 -0500

    Model contacts file created with validations.

---
+++ app/models/contacts_file.rb
@@ -0,0 +1,8 @@
+class ContactsFile < ApplicationRecord
+  belongs_to :user
+  has_one_attached :csv_file
+
+  enum status: { waiting: 0, processing: 1, failed: 2, finished: 3 }
+
+  validates :csv_file, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,54 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_02_191019) do
+ActiveRecord::Schema.define(version: 2019_07_03_134352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.date "birthdate"
+    t.string "phone_number"
+    t.string "address"
+    t.string "credit_card"
+    t.string "franchise"
+    t.string "email"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_contacts_on_user_id"
+  end
+
+  create_table "contacts_files", force: :cascade do |t|
+    t.string "csv_file"
+    t.integer "status", default: 0
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_contacts_files_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,4 +72,7 @@ ActiveRecord::Schema.define(version: 2019_07_02_191019) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "contacts", "users"
+  add_foreign_key "contacts_files", "users"
 end
